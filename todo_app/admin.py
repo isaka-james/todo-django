a823275bb419ebad8c51277433e04b3947ec4242
isaka-james <mr.isakajames@gmail.com>
2024-07-12 14:46:09 +0300

    feature: added a features for admins # features - now admins can see the todos, which previously they only saw users.

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import CustomUser
+from .models import CustomUser, Todo
 
 class CustomUserAdmin(BaseUserAdmin):
     fieldsets = (
@@ -13,7 +13,27 @@ class CustomUserAdmin(BaseUserAdmin):
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
     search_fields = ('username', 'first_name', 'last_name', 'email')
     ordering = ('username',)
-
     filter_horizontal = ('groups', 'user_permissions',)
 
 admin.site.register(CustomUser, CustomUserAdmin)
+
+# Registering Todo model in admin
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner','description', 'created_at', 'expires_at')
+    list_filter = ('owner', 'created_at', 'expires_at', )
+    search_fields = ('title', 'description')
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user  # Assign current user as the owner of the Todo
+        super().save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(owner=request.user)
+
+
