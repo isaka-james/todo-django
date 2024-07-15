71f55e275263df4090759e472743874ede22c494
Masterplan <76619967+isaka-james@users.noreply.github.com>
2024-07-15 14:02:57 +0300

    remove: code smell removed

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,4 +1,3 @@
-# from django.shortcuts import render
 from rest_framework import generics, permissions,status
 from .models import Todo,CustomUser
 from .serializers import TodoSerializer,UserSerializer
